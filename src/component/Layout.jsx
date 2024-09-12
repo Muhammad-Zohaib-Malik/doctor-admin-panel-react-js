eca2a7ba9ecea53b37c99724acf62c49fb677f03
zohaib <muhammadzohaibmalik10@gmail.com>
2024-09-12 17:55:32 +0500

    padding bottom added in section

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -44,7 +44,7 @@ const Layout = ({ children }) => {
   return (
     <div className="bg-[#E9E7EF] min-h-screen">
       <aside
-        className=" fixed top-0 left-0  h-full bg-white overflow-hidden"
+        className="fixed top-0 left-0 h-full overflow-hidden bg-white "
         style={{ width: spacing, transition: "300ms" }}
       >
         <div className="p-5">
@@ -61,7 +61,7 @@ const Layout = ({ children }) => {
               <img className="w-28" src="/images/logo.webp " alt="" />
             ) : null}
           </div>
-          <div className="flex  items-center ">
+          <div className="flex items-center ">
             <img className="w-16" src="/images/avatar.webp" alt="" />
             {spacing === 300 && (
               <h1 className="text-xl font-semibold">Super Admin</h1>
@@ -69,12 +69,12 @@ const Layout = ({ children }) => {
           </div>
         </div>
 
-        <div className="p-3 flex  flex-col gap-2">
+        <div className="flex flex-col gap-2 p-3">
           {menus.map((item, index) => (
             <Link
               to={item.link}
               key={index}
-              className="flex gap-3  px-4 py-2 rounded-md text-black overflow-hidden "
+              className="flex gap-3 px-4 py-2 overflow-hidden text-black rounded-md "
               style={{
                 background: location.pathname === item.link ? "#08976E" : null,
                 color: location.pathname === item.link ? "white" : null,
@@ -87,12 +87,12 @@ const Layout = ({ children }) => {
         </div>
       </aside>
 
-      <section style={{ marginLeft: spacing, transition: "300ms" }}>
-        <nav className="bg-white p-6 shadow flex justify-between">
+      <section className="pb-12" style={{ marginLeft: spacing, transition: "300ms" }}>
+        <nav className="flex justify-between p-6 bg-white shadow">
           <div className="flex items-center">
             <button
               onClick={() => setSpacing(spacing === 300 ? 77 : 300)}
-              className=" w-12 h-12 rounded-full "
+              className="w-12 h-12 rounded-full "
             >
               <MenuIcon />
             </button>
@@ -103,7 +103,7 @@ const Layout = ({ children }) => {
           </div>
 
           <div className="flex items-center gap-8">
-            <button className="flex  gap-1 ">
+            <button className="flex gap-1 ">
               <MessageSquare />
               <h1 className="text-sm">Health Ease</h1>
             </button>
@@ -127,8 +127,8 @@ const Layout = ({ children }) => {
           </div>
         </nav>
         <div className="p-6">{children}</div>
-        <footer className="bg-white p-6 rounded-md m-6 ">
-          <div className="flex items-center  w-fit mx-auto text-gray-600">
+        <footer className="p-6 m-6 bg-white rounded-md ">
+          <div className="flex items-center mx-auto text-gray-600 w-fit">
             <Copyright />
             <p>Zohaibaay | All right reserved</p>
           </div>
